5f00c36cc33933fc5225e743863d63f9311877ef
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-07-26 23:39:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,7 @@
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-answer = {1=>2}
-answer
+answer = {name: 'Grace Hopper'}
 end
 
 def read_from_hash(hash, key)
